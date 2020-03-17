55b08329e582100e5a22a61124df591136600479
subbiah <subbiah@f22labs.com>
2020-03-18 00:36:12 +0530

    Added default routing and components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Router, navigate } from "@reach/router";
+import AskHelp from "./components/AskHelp";
+import SearchHelp from "./components/SearchHelp";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>SumOfUs Aid Tool</p>
+        <div style={{ display: "flex", flexDirection: "row" }}>
+          <button onClick={() => navigate("ask")}>Ask Help</button>
+          <button onClick={() => navigate("search")}>Offer Help</button>
+        </div>
+        <Router basepath="/">
+          <AskHelp path="ask" />
+          <SearchHelp path="search" />
+        </Router>
       </header>
     </div>
   );
