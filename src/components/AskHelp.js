cac89224c927e14f5f62fda182b8e11c90e23f5d
subbiah <subbiah@f22labs.com>
2020-03-18 03:38:09 +0530

    Landing page changes

--- src/components/AskHelp.js
+++ src/components/AskHelp.js
@@ -1,38 +1,51 @@
-import React from "react";
-import { Formik } from "formik";
-import { askSchema } from "../_helper/validationSchema";
-import { countriesList } from "../_helper/countries";
-import Logo from "../sou.svg";
-import { navigate } from "@reach/router";
+import React from 'react'
+import { Formik } from 'formik'
+import { askSchema } from '../_helper/validationSchema'
+import { countriesList } from '../_helper/countries'
+import Logo from '../sou.svg'
+import { navigate } from '@reach/router'
 
 const AskHelp = () => {
   const onFormSubmit = values => {
-    console.log(values);
-  };
+    console.log(values)
+  }
   //   function onCountryChange(selectedCountry) {
   //     formik.setFieldValue("country", selectedCountry);
   //   }
   return (
     <div
       style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-        background: "#282c34"
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        background: '#282c34'
       }}
-    >        
-    <img src={Logo} style={{width: '140px', height: '80px', position: 'absolute', cursor:'pointer', top: '0', left: '0', paddingLeft: '30px'}} onClick={() => navigate('/')} />
-      <div style={{ color: "white", fontSize: "24px", margin: "30px 0" }}>
+    >
+      <img
+        src={Logo}
+        alt='sou-logo'
+        style={{
+          width: '140px',
+          height: '80px',
+          position: 'absolute',
+          cursor: 'pointer',
+          top: '0',
+          left: '0',
+          paddingLeft: '30px'
+        }}
+        onClick={() => navigate('/')}
+      />
+      <div style={{ color: 'white', fontSize: '24px', margin: '30px 0' }}>
         Enter details about the help that you need
       </div>
       <Formik
         initialValues={{
-          name: "",
-          city: "",
-          country: "Afghanistan",
-          content: ""
+          name: '',
+          city: '',
+          country: 'Afghanistan',
+          content: ''
         }}
         validationSchema={askSchema}
         isInitialValid={false}
@@ -46,49 +59,49 @@ const AskHelp = () => {
           values
         }) => (
           <form onSubmit={handleSubmit}>
-            <div className="center ">
-              <div className="row flex">
+            <div className='center '>
+              <div className='row flex'>
                 <input
-                  className="w600"
-                  type="text"
-                  name="name"
+                  className='w600'
+                  type='text'
+                  name='name'
                   onChange={handleChange}
-                  placeholder="Name"
+                  placeholder='Name'
                   required
                 />
               </div>
-              <div className="flex row">
+              <div className='flex row'>
                 <input
-                  className="w600"
-                  type="text"
-                  name="city"
+                  className='w600'
+                  type='text'
+                  name='city'
                   onChange={handleChange}
-                  placeholder="City"
+                  placeholder='City'
                   required
                 />
               </div>
-              <div className="flex row">
-                <select id="country" name="country" onChange={handleChange}>
+              <div className='flex row'>
+                <select id='country' name='country' onChange={handleChange}>
                   {countriesList.map(country => {
                     return (
                       <option value={country.label}>{country.label}</option>
-                    );
+                    )
                   })}
                 </select>
               </div>
-              <div className="flex row">
+              <div className='flex row'>
                 <textarea
                   onChange={handleChange}
-                  className="w600"
-                  placeholder="Enter your content"
-                  name="content"
+                  className='w600'
+                  placeholder='Enter your content'
+                  name='content'
                   required
                   wrap
                   rows={5}
                 />
               </div>
               <div>
-                <button className="btn" type="submit" disabled={!isValid}>
+                <button className='btn' type='submit' disabled={!isValid}>
                   Save
                 </button>
               </div>
@@ -97,7 +110,7 @@ const AskHelp = () => {
         )}
       />
     </div>
-  );
-};
+  )
+}
 
-export default AskHelp;
+export default AskHelp
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,47 +1,60 @@
-import React from "react";
-import { navigate } from "@reach/router";
-import Logo from "../sou.svg";
+import React from 'react'
+import { navigate } from '@reach/router'
+import Logo from '../sou.svg'
+import cover from '../cover.jpg'
 
 const Landing = () => {
   return (
     <>
-      <header className="App-header">
-        <img src={Logo} style={{width: '140px', height: '80px', position: 'absolute', cursor:'pointer', top: '0', left: '0', paddingLeft: '30px'}} onClick={() => navigate('/')} />
-        <p>SumOfUs Aid Tool</p>
-        <img src="" />
-        <div style={{ display: "flex", flexDirection: "row" }}>
+      <header className='App-header'>
+        <img
+          src={Logo}
+          alt='sou-logo'
+          style={{
+            width: '140px',
+            height: '80px',
+            position: 'absolute',
+            cursor: 'pointer',
+            top: '0',
+            left: '0',
+            paddingLeft: '30px'
+          }}
+          onClick={() => navigate('/')}
+        />
+        <p style={{ fontSize: '30px' }}>SumOfUs Aid Tool</p>
+        <div style={{ display: 'flex', flexDirection: 'row' }}>
           <button
             style={{
-              width: "100px",
-              background: "#f8492d",
-              height: "40px",
-              margin: "10px",
-              fontSize: "16px",
-              color: "white",
-              border: "0px"
+              width: '100px',
+              background: '#f8492d',
+              height: '40px',
+              margin: '10px',
+              fontSize: '16px',
+              color: 'white',
+              border: '0px'
             }}
-            onClick={() => navigate("ask")}
+            onClick={() => navigate('ask')}
           >
             Ask Help
           </button>
           <button
             style={{
-              width: "100px",
-              background: "#f8492d",
-              height: "40px",
-              margin: "10px",
-              fontSize: "16px",
-              color: "white",
-              border: "0px"
+              width: '100px',
+              background: '#f8492d',
+              height: '40px',
+              margin: '10px',
+              fontSize: '16px',
+              color: 'white',
+              border: '0px'
             }}
-            onClick={() => navigate("search")}
+            onClick={() => navigate('search')}
           >
             Offer Help
           </button>
         </div>
       </header>
     </>
-  );
-};
+  )
+}
 
-export default Landing;
+export default Landing
